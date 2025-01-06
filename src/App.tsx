0411ec472cf73cfab5781a6a8dd73a28d455ca6c
VasKaleev <vkaleev.fam@gmail.com>
2025-01-06 19:52:31 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Route,Routes } from 'react-router';
+import './App.css';
+import { BestSellers } from './BestSellers';
+import { Header } from './Header';
+import { Product } from './Product';
+
+function App() {
+  return (
+  <div className='appContainer'>
+  <Header/>
+  <Routes>
+        <Route path="/" element={<BestSellers />} />
+        <Route path="/product/:productId" element={<Product />} />
+      </Routes>
+  </div>
+  
+  )
+}
+
+export default App
---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+import cart from './assets/img/cart.svg'
+import logo from './assets/img/logo.svg'
+export const Header = () => {
+    return( 
+    <div className="header">
+    <img src={logo} alt="logo icon" />
+    <img src={cart} alt="cart icon" />
+    </div>
+)
+}
